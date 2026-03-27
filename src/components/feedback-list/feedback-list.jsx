58afd6b3044c67c01e385d3cb708f74c4ea88ee3
IvanaO <82397055+ivanaogrizovic@users.noreply.github.com>
2026-03-27 14:20:37 +0000

    feat: install framer motion and animate feedback list

--- src/components/feedback-list/feedback-list.jsx
+++ src/components/feedback-list/feedback-list.jsx
@@ -1,3 +1,4 @@
+import { motion, animatePresence, AnimatePresence } from "framer-motion";
 import FeedbackItem from "../feedback-item/feedback-item";
 
 export default function FeedbackList({ feedback, handleDelete }) {
@@ -6,9 +7,22 @@ export default function FeedbackList({ feedback, handleDelete }) {
   }
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }

a18db2f3d888555aeeee7a79d0d31486f015a7f6
IvanaO <82397055+ivanaogrizovic@users.noreply.github.com>
2026-04-03 14:36:58 +0100

    feat: fetch feedback from json-server

--- src/context/feedbackContext.js
+++ src/context/feedbackContext.js
@@ -1,21 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "this item is from context",
-      rating: 10,
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  const fetchFeedback = async () => {
+    const response = await fetch("http://localhost:5000/feedback?_sort=id");
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -52,6 +59,7 @@ export const FeedbackProvider = ({ children }) => {
         editFeedback,
         updateFeedback,
         feedbackEdit,
+        isLoading,
       }}
     >
       {children}
